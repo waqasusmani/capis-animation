6e48ae6a3dceb93e2a8216b298ffc1193e3ed8fe
Waqas Usmani <Waqas.Usmani@HQCAA.NET>
2025-04-28 16:57:07 +0500

    initial

---
+++ app.js
@@ -0,0 +1,9 @@
+setTimeout(()=>{
+    document.getElementById('whirl-1').classList.add('whirl-1-circling')
+    document.getElementById('capis').classList.add('capis-visible')
+},3000)
+
+setTimeout(()=>{
+    document.getElementById('whirl-1').classList.remove('whirl-1-circling')
+},10000)
+
